55eb6ca732e5334e0f0c05ef429ec0281aca89f0
Esdra <69021404+esdrasantos@users.noreply.github.com>
2025-12-25 01:57:52 +0000

    DDED: datefixer.py to fix purchase date

---
+++ pdf_pipeline/datefixer.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import constants as const 
+
+def add_purchase_date_column(df):
+
+    dia_mes_ano = df['Data'].str.split(' ')
+    
+    df['Dia'] = dia_mes_ano.str[0].str.strip()
+    df['Mes'] = dia_mes_ano.str[1].str.strip()
+    df['Ano'] = df['Fech. Fatura'].str\
+                .split('-').str[0]\
+                .str.strip()
+    meses = {
+        "JAN": 1,
+        "FEV": 2,
+        "MAR": 3,
+        "ABR": 4,
+        "MAI": 5,
+        "JUN": 6,
+        "JUL": 7,
+        "AGO": 8,
+        "SET": 9,
+        "OUT": 10,
+        "NOV": 11,
+        "DEZ": 12
+    }
+
+    df['N Mes'] = df['Mes'].map(meses)
+
+    df['Data_Compra_Aux'] = df.Ano + "-"\
+                            + df['N Mes'].astype(str) + "-"\
+                            + df.Dia
+    dtdiff = pd.to_datetime(df[const.INVOICECLOSE])-pd.to_datetime(df['Data_Compra_Aux'])
+    df['Dias_Compra'] = dtdiff.dt.days
+    df['Data'] = pd.to_datetime(df['Data_Compra_Aux'])
+    filtro = df['Dias_Compra'] < 0
+    df.loc[filtro, 'Data'] = df.loc[filtro, 'Data'] - pd.DateOffset(years=1)
+    df['Mes Compra'] = df['Data'].dt.month
+    df['Semana Compra'] = df['Data'].dt.isocalendar().week
+    df.drop(columns=['Data_Compra_Aux', 'Dias_Compra'],inplace=True)
